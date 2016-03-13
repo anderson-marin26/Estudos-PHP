86c1dd4dc62d2ff94c2b016eff7d38594b775c7a
Anderson Marin <dam.marinramos@gmail.com>
2016-03-13 20:23:10 -0300

    Working with connections

--- loja/adiciona-produto.php
+++ loja/adiciona-produto.php
@@ -1,9 +1,20 @@
 
-<?php
-	include('cabecalho.php');
-	$nome = $_GET["nome"];
-	$preco = $_GET["preco"];
-?>
+<?php include("cabecalho.php"); ?>
+	<?php 
+	$nome = $_GET['nome'];
+	$preco = $_GET['preco'];
 
-<p class="alert-success">Produto <?= $nome; ?>, <?php echo $preco; ?> adicionado com sucesso!</p>
-<?php include('rodape.php'); ?>
+	//criando variavel com a conexao
+	$conexao = mysqli_connect('localhost','root','','loja');
+	//criando a query para inserir no bd
+	$query = "insert into produtos (nome,preco) values('{$nome}',{$preco})";
+
+	//executando a query
+	if(mysqli_query($conexao,$query)) { ?>
+		<p class="alert-success">Produto <?= $nome ?>, <?= $preco ?> adicionado com sucesso!</p>
+	<?php } else { ?>
+			<p class="alert-danger">Produto <?= $nome ?>, não foi adicionado.</p>
+	<?php
+	}
+?>
+<?php include("rodape.php"); ?>
